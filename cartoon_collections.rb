3a2b800c4b523d0816b5981064cc43e3f184d4e0
Leah Silber <leahsilber@gmail.com>
2019-08-26 23:59:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each do |name|
+  	puts name
+  end
 end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
