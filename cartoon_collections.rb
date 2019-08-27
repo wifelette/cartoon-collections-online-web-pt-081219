ffef58d589ddafed80ce14048cce3c87ed3636a8
Leah Silber <leahsilber@gmail.com>
2019-08-27 00:13:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,9 @@ def long_planeteer_calls(calls_array)
 	end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+	array.find |do| foods
+		cheese_types.include?(food_
+	end
 end
