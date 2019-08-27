cad9aff7b0804a6efb1c4a51140077b97bcfaca1
Leah Silber <leahsilber@gmail.com>
2019-08-27 00:04:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,11 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+		call = call.capitalize
+		"#{call}!"
+	end
 end
 
 def long_planeteer_calls# code an argument here
